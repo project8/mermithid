95eea8fa3575e0336aa232a9d810ed194797d494
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-04-04 15:08:01 -0700

    Repaired Tritium Generator, tested and working

--- tests/Tritium_test.py
+++ tests/Tritium_test.py
@@ -13,16 +13,18 @@ class TritiumTests(unittest.TestCase):
     
     def test_KuriePlot(self):
         from mermithid.processors.TritiumSpectrum import TritiumSpectrumGenerator
+        from mermithid.processors.plots import KuriePlotGeneratorProcessor
         from mermithid.misc.Constants import seconds_per_year, tritium_endpoint
 
         specGen_config = {
-            "volume": 1, # [m3]
-            "density": 1e18, # [1/m3]
-            "experiment duration": 1*seconds_per_year(), # [s]
-            "neutrino mass" :0, # [eV]
-            "energy window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
+            "volume": 7e-6*1e-2, # [m3]
+            "density": 3e17, # [1/m3]
+            "duration": 1.*seconds_per_year()/12., # [s]
+            "neutrino_mass" :0, # [eV]
+            "energy_window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
+            # "energy_window": [0.,tritium_endpoint()+1e3], # [KEmin,KEmax]
             "background": 1e-6, # [counts/eV/s]
-            "energy resolution": 0# [eV]
+            "energy_resolution": 5# [eV]
         }
         specGen = TritiumSpectrumGenerator.TritiumSpectrumGenerator("specGen")
         specGen.Configure(specGen_config)
