e7a05e893e51aa9fda067cb03bbba38044bac844
Mathieu Guigue <mathieu.guigue@pnnl.gov>
2018-06-27 23:21:44 -0700

    Solving #3

--- mermithid/processors/IO/IOCicadaProcessor.py
+++ mermithid/processors/IO/IOCicadaProcessor.py
@@ -1,12 +1,12 @@
 '''
 '''
 
-import CicadaPy
-CicadaPy.loadLibraries(True)
-try:
-    from ROOT import Cicada as KT
-except ImportError:
-    from ROOT import Katydid as KT
+# import CicadaPy
+# CicadaPy.loadLibraries(True)
+# try:
+#     from ROOT import Cicada as KT
+# except ImportError:
+#     from ROOT import Katydid as KT
 
 
 from morpho.processors.IO import IOProcessor
@@ -20,39 +20,17 @@ class IOCicadaProcessor(IOProcessor):
 
     def InternalConfigure(self,params):
         super().InternalConfigure(params)
-        self.tree_name = reader.read_param(params,"tree_name","multiTrackEvents")
-        self.object_name = reader.read_param(params,"object_name","Event")
+        self.object_type = reader.read_param(params,"object_type","TMultiTrackEventData")
+        self.object_name = reader.read_param(params,"object_name","multiTrackEvents:Event")
+        self.use_katydid = reader.read_param(params,"use_katydid",False)
         return True
 
     def Reader(self):
         '''
         '''
         logger.debug("Reading {}".format(self.file_name))
-        file = TFile.Open(self.file_name)
-        if not file:
-            raise FileNotFoundError("File {} does not exist".format(self.file_name))
-
-        # Extract tree from file
-        tree = file.Get(self.tree_name)
-        # Create TTreeReader
-        treeReader = TTreeReader(tree)
-        # Create object TMultiTrackEventData to "point" to the object "Event" in the tree
-        multiTrackEvents = TTreeReaderValue(KT.TMultiTrackEventData)(treeReader, "Event")
-
-        logger.debug("Extracting {} from {}".format(self.variables,self.object_name))
-        theData = {}
-        for var in self.variables:
-            theData.update({str(var): []})
-        # Go through the events
-        while treeReader.Next():
-            for var in self.variables:
-                try:
-                    function = getattr(multiTrackEvents,"Get{}".format(var))
-                except:
-                    logger.error("Variable {} does not exist in TMultiTrackEventData".format(var))
-                    raise
-                theData[var].append(function())
-        self.data = theData
+        from ReadKTOutputFile import ReadKTOutputFile
+        self.data = ReadKTOutputFile(self.file_name,self.variables,katydid=self.use_katydid,objectType=self.object_type,name=self.object_name)
         return True
         
 
--- tests/IO_test.py
+++ tests/IO_test.py
@@ -16,14 +16,15 @@ class IOTests(unittest.TestCase):
         reader_config = {
             "action": "read",
             "filename": "events_rid000001097_000000000_to_000000014.root",
-            "treename": "multiTrackEvents",
-            "objectname": "Event",
+            "object_type": "TMultiTrackEventData",
+            "object_name": "multiTrackEvents:Event",
+            "use_katydid": True,
             "variables": ['StartTimeInAcq','StartFrequency']
         }
         b = IOCicadaProcessor("reader")
         b.Configure(reader_config)
         b.Run()
-        data = b.results
+        data = b.data
         logger.info("Data extracted = {}".format(data.keys()))
         for key in data.keys():
             logger.info("{} -> size = {}".format(key,len(data[key])))
