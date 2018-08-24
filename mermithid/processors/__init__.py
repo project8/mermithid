cdb3496eaa241a06b3ca700bf25d3f4923f3da2b
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-08-24 16:47:23 -0700

    Adding frequency to energy convertor: working and tested

--- mermithid/processors/__init__.py
+++ mermithid/processors/__init__.py
@@ -6,3 +6,4 @@ from __future__ import absolute_import
 from . import IO
 from . import TritiumSpectrum
 from . import plots
+from . import misc
---
+++ mermithid/processors/misc/__init__.py
@@ -0,0 +1,6 @@
+'''
+'''
+
+from __future__ import absolute_import
+
+from .FrequencyEnergyConversionProcessor import FrequencyEnergyConversionProcessor
