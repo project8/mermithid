a1ddce586fdf66b7fa3241b8653fd1563eb14e18
Mathieu Guigue <mgguigue@gmail.com>
2018-10-11 16:44:46 +0200

    Adding Frequency shifter

---
+++ mermithid/processors/misc/FrequencyShifter.py
@@ -0,0 +1,39 @@
+'''
+Processor for shifting frequencies
+Author: M. Guigue
+Date: 10/11/18
+'''
+
+from __future__ import absolute_import
+
+import numpy as np
+
+from morpho.processors import BaseProcessor
+from morpho.utilities import morphologging, reader
+logger=morphologging.getLogger(__name__)
+
+__all__ = []
+__all__.append(__name__)
+
+class FrequencyShifter(BaseProcessor):
+
+    def InternalConfigure(self, params):
+        self.frequencies = list()
+        self.results = list()
+        self.frequency_shift = reader.read_param(params, "frequency_shift", 0)
+        return True
+
+    def InternalRun(self):
+        if len(self.frequencies) == 0:
+            logger.error("No frequencies given")
+            self.results = {}
+            return True
+        if self.frequency_shift == 0:
+            looger.warning("Zero frequency shift!")
+            self.results = self.frequencies
+            return True
+        for frequency in self.frequencies:
+            self.results.append(frequency+self.frequency_shift)
+        return True 
+        
+
