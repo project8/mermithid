c80c14083eaf1cf51ff05f8ef13f1ea98b463b86
Mathieu GUIGUE <mgguigue@gmail.com>
2019-08-08 18:06:34 +0200

    What I have

--- tests/Tritium_test.py
+++ tests/Tritium_test.py
@@ -52,5 +52,91 @@ class TritiumTests(unittest.TestCase):
         histo.Run()
         kurieHisto.Run()
 
+    def test_GenAna(self):
+        from mermithid.processors.TritiumSpectrum import TritiumSpectrumProcessor
+        from morpho.processors.plots import Histogram
+        from mermithid.misc.Constants import seconds_per_year, tritium_endpoint
+
+        energy_resolution = 5
+
+        specGen_config = {
+            "mode": "generate",            
+            "paramRange": {
+                "KE": [18000, 19000]
+            },
+
+            "volume": 7e-6*1e-2, # [m3]
+            "density": 3e17, # [1/m3]
+            "duration": 1.*seconds_per_year()/12., # [s]
+            "neutrino_mass" :0, # [eV]
+            "energy_window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
+            # "energy_window": [0.,tritium_endpoint()+1e3], # [KEmin,KEmax]
+            "background": 1e-6, # [counts/eV/s]
+            # *****
+            # "neutrino_mass" :0, # [eV]
+            # "energy_window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
+            # "energy_window": [0.,tritium_endpoint()+1e3], # [KEmin,KEmax]
+            # "background": 1e-6, # [counts/eV/s]
+            # "n_events": 100,
+            # "n_bkgd": 1022,
+            # *****
+            "energy_resolution": energy_resolution, # [eV],
+            "fixedParams": {
+                "m_nu": 0
+            },
+            "varName": "KE",
+            "iter": 10000,
+            "interestParams": ['KE'],
+        }
+        histo_plot = {
+            "variables": "KE",
+            "n_bins_x": 100,
+            "title": "spectrum"
+        }
+        specFit_config = {
+            "mode": "fit",            
+            "paramRange": {
+                "KE": [18000, 19000]
+            },
+
+            "volume": 7e-6*1e-2, # [m3]
+            "density": 3e17, # [1/m3]
+            "duration": 1.*seconds_per_year()/12., # [s]
+            "neutrino_mass" :0, # [eV]
+            "energy_window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
+            "background": 1e-6, # [counts/eV/s]
+            # *****
+            # "neutrino_mass" :0, # [eV]
+            # "energy_window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
+            # "energy_window": [0.,tritium_endpoint()+1e3], # [KEmin,KEmax]
+            # "background": 1e-6, # [counts/eV/s]
+            # "n_events": 100,
+            # "n_bkgd": 1022,
+            # *****
+            "energy_resolution": energy_resolution, # [eV],
+            "fixedParams": {
+                "m_nu": 0,
+                "widthSmearing": energy_resolution
+            },
+            "varName": "KE",
+            "interestParams": ['KE'],
+            "make_fit_plot": True,
+            "binned": False
+        }
+        specGen = TritiumSpectrumProcessor("specGen")
+        specFit = TritiumSpectrumProcessor("specFit")
+        histo = Histogram("histo")
+
+        specGen.Configure(specGen_config)
+        histo.Configure(histo_plot)
+        specFit.Configure(specFit_config)
+        specGen.Run()
+        result = specGen.data
+        histo.data = result
+        specFit.data = result
+        histo.Run()
+        specFit.Run()
+
+
 if __name__ == '__main__':
     unittest.main()
