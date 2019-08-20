0d8d0b56a875f9e6c227b59ae6bdf7c4fbe18982
Mathieu GUIGUE <mgguigue@gmail.com>
2019-08-20 11:10:09 +0200

    Making Codacy happier

--- tests/Tritium_test.py
+++ tests/Tritium_test.py
@@ -9,22 +9,24 @@ import unittest
 from morpho.utilities import morphologging
 logger = morphologging.getLogger(__name__)
 
+
 class TritiumTests(unittest.TestCase):
-    
+
     def test_KuriePlot(self):
         from mermithid.processors.TritiumSpectrum import TritiumSpectrumGenerator, KuriePlotFitter
         from morpho.processors.plots import Histogram
         from mermithid.misc.Constants import seconds_per_year, tritium_endpoint
 
         specGen_config = {
-            "volume": 7e-6*1e-2, # [m3]
-            "density": 3e17, # [1/m3]
-            "duration": 1.*seconds_per_year()/12., # [s]
-            "neutrino_mass" :0, # [eV]
-            "energy_window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
+            "volume": 7e-6*1e-2,  # [m3]
+            "density": 3e17,  # [1/m3]
+            "duration": 1.*seconds_per_year()/12.,  # [s]
+            "neutrino_mass": 0,  # [eV]
+            # [KEmin,KEmax]
+            "energy_window": [tritium_endpoint()-1e3, tritium_endpoint()+1e3],
             # "energy_window": [0.,tritium_endpoint()+1e3], # [KEmin,KEmax]
-            "background": 1e-6, # [counts/eV/s]
-            "energy_resolution": 5# [eV]
+            "background": 1e-6,  # [counts/eV/s]
+            "energy_resolution": 5  # [eV]
         }
         histo_plot = {
             "variables": "KE",
@@ -60,27 +62,19 @@ class TritiumTests(unittest.TestCase):
         energy_resolution = 5
 
         specGen_config = {
-            "mode": "generate",            
+            "mode": "generate",
             "paramRange": {
                 "KE": [18000, 19000]
             },
 
-            "volume": 7e-6*1e-2, # [m3]
-            "density": 3e17, # [1/m3]
-            "duration": 1.*seconds_per_year()/12., # [s]
-            "neutrino_mass" :0, # [eV]
-            "energy_window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
-            # "energy_window": [0.,tritium_endpoint()+1e3], # [KEmin,KEmax]
-            "background": 1e-6, # [counts/eV/s]
-            # *****
-            # "neutrino_mass" :0, # [eV]
-            # "energy_window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
-            # "energy_window": [0.,tritium_endpoint()+1e3], # [KEmin,KEmax]
-            # "background": 1e-6, # [counts/eV/s]
-            # "n_events": 100,
-            # "n_bkgd": 1022,
-            # *****
-            "energy_resolution": energy_resolution, # [eV],
+            "volume": 7e-6*1e-2,  # [m3]
+            "density": 3e17,  # [1/m3]
+            "duration": 1.*seconds_per_year()/12.,  # [s]
+            "neutrino_mass": 0,  # [eV]
+            # [KEmin,KEmax]
+            "energy_window": [tritium_endpoint()-1e3, tritium_endpoint()+1e3],
+            "background": 1e-6,  # [counts/eV/s]
+            "energy_resolution": energy_resolution,  # [eV],
             "fixedParams": {
                 "m_nu": 0
             },
@@ -94,26 +88,18 @@ class TritiumTests(unittest.TestCase):
             "title": "spectrum"
         }
         specFit_config = {
-            "mode": "fit",            
+            "mode": "fit",
             "paramRange": {
                 "KE": [18000, 19000]
             },
-
-            "volume": 7e-6*1e-2, # [m3]
-            "density": 3e17, # [1/m3]
-            "duration": 1.*seconds_per_year()/12., # [s]
-            "neutrino_mass" :0, # [eV]
-            "energy_window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
-            "background": 1e-6, # [counts/eV/s]
-            # *****
-            # "neutrino_mass" :0, # [eV]
-            # "energy_window": [tritium_endpoint()-1e3,tritium_endpoint()+1e3], # [KEmin,KEmax]
-            # "energy_window": [0.,tritium_endpoint()+1e3], # [KEmin,KEmax]
-            # "background": 1e-6, # [counts/eV/s]
-            # "n_events": 100,
-            # "n_bkgd": 1022,
-            # *****
-            "energy_resolution": energy_resolution, # [eV],
+            "volume": 7e-6*1e-2,  # [m3]
+            "density": 3e17,  # [1/m3]
+            "duration": 1.*seconds_per_year()/12.,  # [s]
+            "neutrino_mass": 0,  # [eV]
+            # [KEmin,KEmax]
+            "energy_window": [tritium_endpoint()-1e3, tritium_endpoint()+1e3],
+            "background": 1e-6,  # [counts/eV/s]
+            "energy_resolution": energy_resolution,  # [eV],
             "fixedParams": {
                 "m_nu": 0,
                 "widthSmearing": energy_resolution
