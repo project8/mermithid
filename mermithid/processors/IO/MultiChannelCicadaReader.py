d81e95c812132ac2bcd9052ece6c4344f02badd4
cclaessens <claessen@uni-mainz.de>
2020-04-08 17:38:01 +0200

    added MultiChannelCicadaReader and test script

---
+++ mermithid/processors/IO/MultiChannelCicadaReader.py
@@ -0,0 +1,77 @@
+'''
+Processor that can read (not write) Katydid ROOT files using the Cicada library
+Author: C. Claessens
+Date: April 08 2020
+'''
+
+from morpho.processors.IO import IOProcessor
+from morpho.utilities import reader, morphologging
+logger = morphologging.getLogger(__name__)
+
+try:
+    from ROOT import TFile, TTreeReader, TTreeReaderValue
+except ImportError:
+    pass
+
+import numpy as np
+
+class MultiChannelCicadaReader(IOProcessor):
+    '''
+    Processor that can read (not write) Katydid ROOT files using the Cicada library
+    '''
+
+    def InternalConfigure(self,params):
+        '''
+        Args:
+            object_type: class of the object to read in the file
+            object_name: name of the tree followed by the name of the object
+            use_katydid: retro-compatibility to Katydid namespace
+        '''
+        super().InternalConfigure(params)
+        self.object_type = reader.read_param(params,"object_type","TMultiTrackEventData")
+        self.object_name = reader.read_param(params,"object_name","multiTrackEvents:Event")
+        self.use_katydid = reader.read_param(params,"use_katydid",False)
+
+        self.N_channels = reader.read_param(params, "N_channels", 3)
+        self.channel_ids = reader.read_param(params, "channel_ids", ['a', 'b', 'c'])
+        self.rf_roi_min_freqs = reader.read_param(params, "rf_roi_min_freqs", [0, 0, 0])
+        self.transition_freqs = reader.read_param(params, "channel_transition_freqs", [0, 0])
+        return True
+
+    def Reader(self):
+        '''
+        Reader method
+        '''
+        print('\nReading data from {} channels'.format(self.N_channels))
+        print('Min frequencies: {}'.format(self.rf_roi_min_freqs))
+        print('Transition freqs: {}'.format(self.transition_freqs))
+
+        self.data = {}
+        for i in range(self.N_channels):
+            logger.debug("Reading {}".format(self.file_name[i]))
+            try:
+                from ReadKTOutputFile import ReadKTOutputFile
+            except ImportError:
+                logger.warn("Cannot import ReadKTOutputFile")
+            self.data[self.channel_ids[i]] = ReadKTOutputFile(self.file_name[i],self.variables,katydid=self.use_katydid,objectType=self.object_type,name=self.object_name)
+
+
+        if 'StartFrequency' in self.variables:
+            all_frequencies = []
+
+            for i in range(self.N_channels):
+                true_frequencies = np.array(self.data[self.channel_ids[i]]['StartFrequency'])+self.rf_roi_min_freqs[i]
+                index = np.where((true_frequencies>=self.transition_freqs[i][0]) &(true_frequencies<self.transition_freqs[i][1]))
+                all_frequencies.extend(list(true_frequencies[index]))
+
+                self.data[self.channel_ids[i]]['TrueStartFrequenciesCut'] = true_frequencies[index]
+
+            self.data['TrueStartFrequenciesMerged'] = all_frequencies
+        return True
+
+    def Writer(self):
+        '''
+        End-user analysis should not produce Katydid output objects...
+        '''
+        logger.error("End user analysis: cannot write reconstruction algorithm output")
+        raise
---
+++ tests/MultiChannelCicadaReader_test.py
@@ -0,0 +1,58 @@
+'''
+This scripts aims at testing IO processors by reading files.
+Author: M. Guigue
+Date: Feb 27 2018
+'''
+
+import unittest
+
+from morpho.utilities import morphologging
+logger = morphologging.getLogger(__name__)
+
+import matplotlib.pyplot as plt
+
+class IOTests(unittest.TestCase):
+
+    def test_Cicada(self):
+        from mermithid.processors.IO import MultiChannelCicadaReader
+        reader_config = {
+            "action": "read",
+            "N_channels": 3,
+            "filename": ["/host/input_data/tritium_run_3_channel_a_some_runs.root",
+                         "/host/input_data/tritium_run_3_channel_b_some_runs.root",
+                         "/host/input_data/tritium_run_3_channel_c_some_runs.root"],
+            "rf_roi_min_freqs": [25803125000.0, 25862500000.0, 25921875000.0],
+            "channel_transition_freqs": [[0,1.38623121e9+24.5e9],
+                                         [1.38623121e9+24.5e9, 1.44560621e9+24.5e9],
+                                        [1.44560621e9+24.5e9, 50e9]],
+            "object_type": "TMultiTrackEventData",
+            "object_name": "multiTrackEvents:Event",
+            "use_katydid": False,
+            "variables": ['StartFrequency', 'StartTimeInRunC']
+        }
+        b = MultiChannelCicadaReader("reader")
+        b.Configure(reader_config)
+        b.Run()
+        data = b.data
+        logger.info("Data extracted = {}".format(data.keys()))
+        for key in data.keys():
+            logger.info("{} -> size = {}".format(key,len(data[key])))
+
+        print(data.keys())
+
+        plt.figure(figsize=(7,7))
+        plt.subplot(211)
+        n, bins, p = plt.hist(data['TrueStartFrequenciesMerged'], bins=100)
+        plt.xlabel('Start frequencies')
+        plt.ylabel('N')
+
+        plt.subplot(212)
+        plt.hist(data['a']['TrueStartFrequenciesCut'], bins=bins, label='channel a')
+        plt.hist(data['b']['TrueStartFrequenciesCut'], bins=bins, label='channel a')
+        plt.hist(data['c']['TrueStartFrequenciesCut'], bins=bins, label='channel a')
+        plt.xlabel('Start frequencies')
+        plt.ylabel('N')
+        plt.savefig('multi_channel_reader_test.png', dpi=200)
+
+if __name__ == '__main__':
+    unittest.main()
