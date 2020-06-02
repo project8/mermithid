9b0813c6605d2656f4ce3dac70fa439497b53172
cclaessens <claessen@uni-mainz.de>
2020-06-02 12:00:13 +0200

    changed plot path to relative and removed unittest usage

--- test_analysis/Complex_line_shape_fitter.py
+++ test_analysis/Complex_line_shape_fitter.py
@@ -5,78 +5,74 @@ Date: 4/8/20
 '''
 
 import numpy as np
-import unittest
 import matplotlib.pyplot as plt
 
 from morpho.utilities import morphologging
 logger = morphologging.getLogger(__name__)
 
-class ComplexLineShapeTests(unittest.TestCase):
-
-    def test_complex_lineshape(self):
-        from mermithid.processors.IO import IOCicadaProcessor
-        from mermithid.processors.misc.ComplexLineShape import ComplexLineShape
+def test_complex_lineshape():
+    from mermithid.processors.IO import IOCicadaProcessor
+    from mermithid.processors.misc.ComplexLineShape import ComplexLineShape
 
 
-        reader_config = {
-            "action": "read",
-            "filename": "/host-mermithid/tests/events_000007031_katydid_v2.13.0_concat.root",
-            "object_type": "TMultiTrackEventData",
-            "object_name": "multiTrackEvents:Event",
-            "use_katydid": False,
-            "variables": ['StartTimeInAcq','StartFrequency']
-        }
-        complexLineShape_config = {
-            'bins_choice': np.linspace(0,90e6,1000),
-            'gases': ["H2","Kr"],
-            'max_scatters': 20,
-            'fix_scatter_proportion': True,
-            'gas1_scatter_proportion': 0.8,
-            # This is an important parameter which determines how finely resolved
-            # the scatter calculations are. 10000 seems to produce a stable fit, with minimal slowdown
-            'num_points_in_std_array': 10000,
-            'RF_ROI_MIN': 25850000000.0,
-            'B_field': 0.957810722501,
-            'shake_spectrum_parameters_json_path': '/host/shake_spectrum_parameters.json',
-            'path_to_osc_strengths_files': '/host/'
-        }
+    reader_config = {
+        "action": "read",
+        "filename": "/host-mermithid/tests/events_000007031_katydid_v2.13.0_concat.root",
+        "object_type": "TMultiTrackEventData",
+        "object_name": "multiTrackEvents:Event",
+        "use_katydid": False,
+        "variables": ['StartTimeInAcq','StartFrequency']
+    }
+    complexLineShape_config = {
+        'bins_choice': np.linspace(0,90e6,1000),
+        'gases': ["H2","Kr"],
+        'max_scatters': 20,
+        'fix_scatter_proportion': True,
+        'gas1_scatter_proportion': 0.8,
+        # This is an important parameter which determines how finely resolved
+        # the scatter calculations are. 10000 seems to produce a stable fit, with minimal slowdown
+        'num_points_in_std_array': 10000,
+        'RF_ROI_MIN': 25850000000.0,
+        'B_field': 0.957810722501,
+        'shake_spectrum_parameters_json_path': '/host/shake_spectrum_parameters.json',
+        'path_to_osc_strengths_files': '/host/'
+    }
 
-        b = IOCicadaProcessor("reader")
-        complexLineShape = ComplexLineShape("complexLineShape")
+    b = IOCicadaProcessor("reader")
+    complexLineShape = ComplexLineShape("complexLineShape")
 
-        b.Configure(reader_config)
-        complexLineShape.Configure(complexLineShape_config)
+    b.Configure(reader_config)
+    complexLineShape.Configure(complexLineShape_config)
 
-        b.Run()
-        data = b.data
-        logger.info("Data extracted = {}".format(data.keys()))
-        for key in data.keys():
-            logger.info("{} -> size = {}".format(key,len(data[key])))
+    b.Run()
+    data = b.data
+    logger.info("Data extracted = {}".format(data.keys()))
+    for key in data.keys():
+        logger.info("{} -> size = {}".format(key,len(data[key])))
 
-        #print(data['StartFrequency'])
-        start_frequency_array = np.array(data['StartFrequency'])
+    #print(data['StartFrequency'])
+    start_frequency_array = np.array(data['StartFrequency'])
 
-        complexLineShape.data = data
+    complexLineShape.data = data
 
-        complexLineShape.Run()
+    complexLineShape.Run()
 
-        results = complexLineShape.results
-        print(results['output_string'])
-#        print(type(results['data_hist_freq']))
-#        print(len(results['data_hist_freq']))
+    results = complexLineShape.results
+    print(results['output_string'])
 
-        # plot fit with shake spectrum
-        plt.rcParams.update({'font.size': 20})
-        plt.figure(figsize=(15,9))
-        plt.step(
-        results['bins_Hz'][0:-1]/1e9, results['data_hist_freq'],
-        label = 'data\n total counts = {}\n'.format(len(data['StartFrequency']))
-        )
-        plt.plot(results['bins_Hz'][0:-1]/1e9, results['fit_Hz'], label = results['output_string'], alpha = 0.7)
-        plt.legend(loc = 'upper left',fontsize = 12)
-        plt.xlabel('frequency GHz')
-        plt.title('fit with shake spectrum 2 gas scattering')
-        plt.savefig('/host/plots/fit_shake_2_gas_0.png')
+
+    # plot fit with shake spectrum
+    plt.rcParams.update({'font.size': 20})
+    plt.figure(figsize=(15,9))
+    plt.step(
+    results['bins_Hz'][0:-1]/1e9, results['data_hist_freq'],
+    label = 'data\n total counts = {}\n'.format(len(data['StartFrequency']))
+    )
+    plt.plot(results['bins_Hz'][0:-1]/1e9, results['fit_Hz'], label = results['output_string'], alpha = 0.7)
+    plt.legend(loc = 'upper left',fontsize = 12)
+    plt.xlabel('frequency GHz')
+    plt.title('fit with shake spectrum 2 gas scattering')
+    plt.savefig('fit_shake_2_gas_0.png')
 
 if __name__ == '__main__':
-    unittest.main()
+    test_complex_lineshape()
