e733607476f8117f2c8852ee73df663ccc9d40fb
cclaessens <claessens.christine@hotmail.de>
2020-06-13 20:22:35 +0200

    iminuit binned data fitter

---
+++ mermithid/processors/misc/BinnedDataFitter.py
@@ -0,0 +1,152 @@
+'''
+Author: C. Claessens
+Date:6/12/2020
+Description: Configure with arbitrary histogram and pdf.
+Minimizes poisson loglikelihood using iMinuit.
+
+'''
+
+from __future__ import absolute_import
+
+import numpy as np
+import scipy
+import sys
+import json
+from iminuit import Minuit
+from morpho.utilities import morphologging, reader
+from morpho.processors import BaseProcessor
+
+logger = morphologging.getLogger(__name__)
+
+
+
+__all__ = []
+__all__.append(__name__)
+
+class BinnedDataFitter(BaseProcessor):
+    '''
+    Processor that
+    Args:
+        variables
+        parameter_names
+        initial_values
+        limits
+        fixed
+        bins
+        print_level
+        constrained_parameter_indices
+        constrained_parameter_means
+        constrained_parameter_widths
+
+    Inputs:
+        data:
+    Output:
+        result: dictionary containing
+    '''
+    def InternalConfigure(self, params):
+        '''
+        Configure
+        '''
+
+        # Read other parameters
+        self.namedata = reader.read_param(params, 'variables', "required")
+        self.parameter_names = reader.read_param(params, 'parameter_names', ['A', 'mu', 'sigma'])
+        self.initial_values = reader.read_param(params, 'initial_values', [1, 0, 1])
+        self.limits = reader.read_param(params, 'limits', [[None, None], [None, None], [None, None]])
+        self.fixes = reader.read_param(params, 'fixed', [False, False, False])
+        self.bins = reader.read_param(params, 'bins', np.linspace(-2, 2, 100))
+        self.print_level = reader.read_param(params, 'print_level', 1)
+
+        self.constrained_parameters = reader.read_param(params, 'constrained_parameter_indices', [])
+        self.constrained_means = reader.read_param(params, 'constrained_parameter_means', [])
+        self.constrained_widths = reader.read_param(params, 'constrained_parameter_widths', [])
+
+        # derived configurations
+        self.bin_centers = self.bins[0:-1]+0.5*(self.bins[1]-self.bins[0])
+        self.parameter_errors = [max([0.1, 0.1*p]) for p in self.initial_values]
+
+        return True
+
+    def InternalRun(self):
+        logger.info('namedata: {}'.format(self.namedata))
+
+        self.hist, _ = np.histogram(self.data[self.namedata], self.bins)
+
+
+        # Now minimize neg log likelihood using iMinuit
+        logger.info('This is the plan:')
+        logger.info('Fitting data consisting of {} elements'.format(np.sum(self.hist)))
+        logger.info('Fit parameters: {}'.format(self.parameter_names))
+        logger.info('Initial values: {}'.format(self.initial_values))
+        logger.info('Initial error: {}'.format(self.parameter_errors))
+        logger.info('Fixed in fit: {}'.format(self.fixes))
+        logger.info('Print level: {}'.format(self.print_level))
+        logger.info('Constrained parameters: {}'.format([self.parameter_names[i] for i in self.constrained_parameters]))
+
+        m_binned = Minuit.from_array_func(self.negPoissonLogLikelihood,
+                                      self.initial_values,
+                                      error=self.parameter_errors,
+                                      errordef = 0.5, limit = self.limits,
+                                      name=self.parameter_names,
+                                      fix=self.fixes, print_level=self.print_level)
+
+
+        # minimze
+        m_binned.migrad(resume=False)
+        result_array = m_binned.np_values()
+        error_array = m_binned.np_errors()
+
+        # save results
+        self.results = {}
+        self.results['param_values'] = result_array
+        self.results['param_errors'] = error_array
+        for i, k in enumerate(self.parameter_names):
+            self.results[k] = {'value': result_array[i], 'error': error_array[i]}
+
+
+        return True
+
+    def PDF(self, x, A, mu, sigma):
+        """
+        Overwrite by whatever PDF
+        """
+        f = A*(1/(sigma*np.sqrt(2*np.pi)))*np.exp(-(((x-mu)/sigma)**2.)/2.)
+        return f
+
+
+    def PoissonLogLikelihood(self, params):
+
+        # binned data
+        hist = self.hist
+
+        # expectation
+        pdf_return = self.PDF(self.bin_centers, *params)
+        if np.shape(pdf_return)[0] == 2:
+            expectation, expectation_error = pdf_return
+        else:
+            expectation = pdf_return
+
+        # exclude bins where expectation is <= zero or nan
+        index = np.where(expectation>0)
+
+        # poisson log likelihoood
+        ll = (hist[index]*np.log(expectation[index]) - expectation[index]).sum()
+
+        # extended ll: poisson total number of events
+        N = np.nansum(expectation)
+        extended_ll = -N+np.sum(hist)*np.log(N)+ll
+        return extended_ll
+
+
+    def negPoissonLogLikelihood(self, params):
+
+        # neg log likelihood
+        neg_ll = - self.PoissonLogLikelihood(params)
+
+        # constrained parameters
+        if len(self.constrained_parameters) > 0:
+            for i in range(len(self.constrained_parameters)):
+                i_param = self.constrained_parameters[i]
+                neg_ll += 0.5 * ((params[i_param] - self.constrained_means[i])/ self.constrained_widths[i])**2
+
+        return neg_ll
--- tests/Misc_test.py
+++ tests/Misc_test.py
@@ -8,27 +8,99 @@ Converts frequencies corresponding to kinetic energy
 
 import unittest
 
-from morpho.utilities import morphologging, read_param
+from morpho.utilities import morphologging, parser
 logger = morphologging.getLogger(__name__)
 
+import matplotlib.pyplot as plt
+import numpy as np
 
-class TritiumTests(unittest.TestCase):
-    from mermithid.processors.misc import FrequencyEnergyConversionProcessor
-    
-    freq_data = [27.9925*10**9, 27.0094*10**9,
-                 26.4195*10**9, 26.4169*10**9,
-                 26.3460*10**9, 26.3457*10**9]
-    logger.info("Will convert the following frequencies: %s"%freq_data)
-    logger.debug("At 1 T, these correspond to kinetic energies (in keV) of " +
-          "[0, 18.6, 30.424, 30.477, 31.934, 31.942]")
-                 
-    freq_energy_dict = {
-        "B": 1
-    }
+class MiscTest(unittest.TestCase):
 
-    freq_proc = FrequencyEnergyConversionProcessor("freq_energy_processor")
-    freq_proc.Configure(freq_energy_dict)
-    freq_proc.frequencies = freq_data
-    freq_proc.Run()
+    def test_FreqConversionTest(self):
+        from mermithid.processors.misc import FrequencyEnergyConversionProcessor
 
-    logger.info("Resulting energies: %s"%freq_proc.energies)
+        freq_data = [27.9925*10**9, 27.0094*10**9,
+                     26.4195*10**9, 26.4169*10**9,
+                     26.3460*10**9, 26.3457*10**9]
+        logger.info("Will convert the following frequencies: %s"%freq_data)
+        logger.debug("At 1 T, these correspond to kinetic energies (in keV) of " +
+              "[0, 18.6, 30.424, 30.477, 31.934, 31.942]")
+
+        freq_energy_dict = {
+            "B": 1
+        }
+
+        freq_proc = FrequencyEnergyConversionProcessor("freq_energy_processor")
+        freq_proc.Configure(freq_energy_dict)
+        freq_proc.frequencies = freq_data
+        freq_proc.Run()
+
+        logger.info("Resulting energies: %s"%freq_proc.energies)
+
+
+    def test_BinnedDataFitter(self):
+        from mermithid.processors.misc import BinnedDataFitter
+
+        logger.info('iMinuit fit test')
+        config_dict = {
+            'variables': 'K',
+            'bins': np.linspace(-3, 3, 60),
+            'parameter_names': ['A', 'mu', 'sigma'],
+            'initial_values': [100, 0, 1],
+            'limits': [[0, None], [None, None], [0, None]],
+            'constrained_parameter_indices': [1],
+            'constrained_parameter_means': [0],
+            'constrained_parameter_widths': [0.1]
+
+            }
+
+        random_data = {'K': np.random.randn(1000)*0.5+1}
+        negll_fitter = BinnedDataFitter('iminuit_processor')
+        negll_fitter.Configure(config_dict)
+        negll_fitter.data = random_data
+        negll_fitter.Run()
+
+        results = negll_fitter.results
+
+        for k in results.keys():
+            logger.info('{}: {}'.format(k, results[k]))
+
+        x = negll_fitter.bin_centers
+        hist = negll_fitter.hist
+        result_list = results['param_values']
+        error_list = results['param_errors']
+
+        plt.rcParams.update({'font.size': 20})
+        plt.figure(figsize=(10,10))
+        plt.subplot(211)
+        plt.errorbar(x, hist, np.sqrt(hist), drawstyle='steps-mid', label='Binned data')
+        plt.plot(x, negll_fitter.PDF(x, *result_list), label='fit')
+        plt.xlabel(negll_fitter.namedata)
+        plt.ylabel('Counts')
+
+        plt.subplot(212)
+        plt.scatter(x, (hist-negll_fitter.PDF(x, *result_list))/np.sqrt(hist), label='Pearson residuals')
+        plt.xlabel(negll_fitter.namedata)
+        plt.ylabel('Residuals')
+
+
+        plt.savefig('iminit_fit.png')
+
+
+
+
+if __name__ == '__main__':
+
+    args = parser.parse_args(False)
+
+
+    logger = morphologging.getLogger('morpho',
+                                     level=args.verbosity,
+                                     stderr_lb=args.stderr_verbosity,
+                                     propagate=False)
+    logger = morphologging.getLogger(__name__,
+                                     level=args.verbosity,
+                                     stderr_lb=args.stderr_verbosity,
+                                     propagate=False)
+
+    unittest.main()
