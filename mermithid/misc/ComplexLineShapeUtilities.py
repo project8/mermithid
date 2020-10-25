c86d9ee0af4648e0213dcb0837d5d44ff43d73b0
Xueying-Huyan <huyanxy@gmail.com>
2020-10-24 20:14:37 -0700

    updated get_eloss_spec function in ComplexLineShapeUtilities.py

--- mermithid/misc/ComplexLineShapeUtilities.py
+++ mermithid/misc/ComplexLineShapeUtilities.py
@@ -66,10 +66,11 @@ def aseev_func_tail(energy_loss_array, gas_type):
 
 #convert oscillator strength into energy loss spectrum
 def get_eloss_spec(e_loss, oscillator_strength, kr_17keV_line): #energies in eV
-    kinetic_en = kr_17keV_line * 1000
+    kinetic_en = kr_17keV_line
     e_rydberg = 13.605693009 #rydberg energy (eV)
     a0 = 5.291772e-11 #bohr radius
-    return np.where(e_loss>0 , 4.*np.pi*a0**2 * e_rydberg / (kinetic_en * e_loss) * oscillator_strength * np.log(4. * kinetic_en * e_loss / (e_rydberg**3.) ), 0)
+    argument_of_log = np.where(e_loss > 0, 4. * kinetic_en * e_rydberg / (e_loss**2.) , 1e-5)
+    return np.where(e_loss>0 , 1./(e_loss) * oscillator_strength* np.log(argument_of_log), 0)
 
 # Takes only the nonzero bins of a histogram
 def get_only_nonzero_bins(bins,hist):
