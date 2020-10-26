8a2079a0558799f1796d48c0cd44e63575c791fc
Mathieu GUIGUE <mgguigue@gmail.com>
2020-10-26 19:02:49 +0100

    Adding a iminiut super simple tester which should pass tests while the processor test doesn't...

--- tests/Fitters_test.py
+++ tests/Fitters_test.py
@@ -15,6 +15,22 @@ import numpy as np
 
 class FittersTest(unittest.TestCase):
 
+    def test_iminuit(self):
+        logger.info('iMinuit test')
+        from iminuit import Minuit
+
+        def f(x, y, z):
+            return (x - 2) ** 2 + (y - 3) ** 2 + (z - 4) ** 2
+
+        m = Minuit(f)
+
+        m.migrad()  # run optimiser
+        print(m.values)  # {'x': 2,'y': 3,'z': 4}
+
+        m.hesse()   # run covariance estimator
+        print(m.errors)  # {'x': 1,'y': 1,'z': 1}
+
+        logger.info('iMinuit test done')
 
     def test_BinnedDataFitter(self):
         from mermithid.processors.Fitters import BinnedDataFitter
