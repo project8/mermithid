8d8b02dd5110706a09ccf35f7b3fe535df61847a
cclaessens <claessens.christine@hotmail.de>
2021-01-26 13:53:00 +0100

    example implementations for tutorial

--- mermithid/processors/Fitters/LeastSquaresPolynomialFitter.py
+++ mermithid/processors/Fitters/LeastSquaresPolynomialFitter.py
@@ -8,6 +8,9 @@ Description:
 
 from __future__ import absolute_import
 
+import numpy as np
+from scipy.optimize import curve_fit
+
 from morpho.utilities import morphologging, reader
 from morpho.processors import BaseProcessor
 
@@ -31,18 +34,75 @@ class LeastSquaresPolynomialFitter(BaseProcessor):
         result: dictionary containing fit results and uncertainties
     '''
     def InternalConfigure(self, params):
+        '''
+        Args:
+            x_key (required): key for data x coordinates
+            y_key (required): key for data y coordinates
+            error_key (optional): key for data y uncertainties. default '' is assumed to mean no uncertainties are provided.
+            order (optional): polynomial order to be fitted. default is 1.
+            initial_guess (optional): list with initial guesses. length has to match order. defaults are 1.
+        '''
 
+        self.x_key = reader.read_param(params, 'x_key', 'required')
+        self.y_key = reader.read_param(params, 'y_key', 'required')
+        self.error_key = reader.read_param(params, 'error_key', '')
+        self.order = reader.read_param(params, 'order', 1)
+        self.initial_guess = reader.read_param(params, 'initial_guess', np.ones(self.order+1))
+
+        # do checks
+        if len(self.initial_guess) != self.order+1:
+            logger.error('Length of inital guess list does not match poylnomial order')
+            return False
+
+        # derived configurations
+        if self.error_key != '':
+            self.fit_with_uncertainties = True
+        else:
+            self.fit_with_uncertainties = False
 
         return True
 
 
     def InternalRun(self):
         '''
-        Description
+        Fits polynomial to data
         '''
+        x = self.data[self.x_key]
+        y = self.data[self.y_key]
 
-        self.results = {}
+        if self.fit_with_uncertainties:
+            y_error = self.data[self.error_key]
+            absolute_sigma = True
+        else:
+            y_error = None
+            absolute_sigma = False
+
+        # now fit
+        pars, cov = curve_fit(self.fit_function, x, y, sigma=y_error,
+                              p0=self.initial_guess, absolute_sigma=absolute_sigma)
+
+        sorted_x = np.sort(x)
+        self.results = {'sorted_x': sorted_x, 'best_fit': self.fit_function(sorted_x, *pars),
+                        'coefficients': pars, 'covariance_matrix': cov}
 
         return True
+
+    def fit_function(self, x, *p):
+        '''
+        Parameters
+        ----------
+        x : numpy array
+        p : list or array
+            coefficients
+
+        Returns
+        -------
+        fit_result
+        '''
+
+        fit_result = np.zeros(np.shape(x))
+        for i in range(len(p)):
+            fit_result += p[i]*np.array(x)**i
+        return fit_result
 
 
--- tests/Polynomial_fitter_test.py
+++ tests/Polynomial_fitter_test.py
@@ -17,6 +17,7 @@ class FittersTest(unittest.TestCase):
         import matplotlib.pyplot as plt
         import numpy as np
         from mermithid.processors.Fitters import LeastSquaresPolynomialFitter
+        from mermithid.processors.TritiumSpectrum import FakeDataGenerator
 
         logger.info('Polynomial fit test')
 
@@ -34,29 +35,101 @@ class FittersTest(unittest.TestCase):
 
         # config dict
         config_dict = {
-            'order': 1,
+            'order': 2,
             'x_key': 'x',
-            'y_key': 'y'
+            'y_key': 'y',
+            'error_key': 'y_error'
             }
 
 
         # setup processor
+        poly_fitter = LeastSquaresPolynomialFitter('poly_fitter')
+        poly_fitter.Configure(config_dict)
 
         # before running give data to processor
+        poly_fitter.data = {'x': random_x_values, 'y':random_y_values, 'y_error': y_uncertainty}
 
         # run processor
+        poly_fitter.Run()
 
         # collect results
+        results = poly_fitter.results
+        logger.info('Fit results:')
+        logger.info(results)
 
 
         # plot
         plt.figure()
         plt.errorbar(random_x_values, random_y_values, y_uncertainty, fmt='o', label='data')
+        plt.plot(results['sorted_x'], results['best_fit'], label='Fit result')
+        plt.title('Fitted coefficients: {}'.format(np.round(results['coefficients'],3)))
         plt.xlabel('x')
         plt.ylabel('y')
         plt.legend(loc='best')
         plt.tight_layout
         plt.savefig('polynomial_fitter_test.png')
+
+
+        #######################################################################
+        # repeat using FakeDataGenerator
+        specGen_config = {
+            "apply_efficiency": False,
+            "efficiency_path": "./combined_energy_corrected_eff_at_quad_trap_frequencies.json",
+            "simplified_lineshape_path": None,
+            "path_to_detailed_scatter_spectra_dir": '/host',
+            "detailed_or_simplified_lineshape": "detailed", #"simplified" or "detailed"
+            "use_lineshape": False, # if False only gaussian smearing is applied
+            "return_frequency": True,
+            "scattering_sigma": 18.6, # only used if use_lineshape = True
+            "survival_prob": 0.77, # only used if use_lineshape = True
+            "scatter_proportion": 0.8, # only used if use_lineshape = True and lineshape = detailed
+            "B_field": 0.9578186017836624,
+            "S": 4500, # number of tritium events
+            "n_steps": 10000, # stepsize for pseudo continuous data is: (Kmax_eff-Kmin_eff)/nsteps
+            "A_b": 1e-10, # background rate 1/eV/s
+            "poisson_stats": True,
+            "molecular_final_states": False,
+            "final_states_file": "../mermithid/misc/saenz_mfs.json"
+        }
+
+        specGen = FakeDataGenerator("specGen")
+
+        specGen.Configure(specGen_config)
+
+        specGen.Run()
+        results = specGen.results
+
+        # plot histograms of generated data
+        Kgen = results['K']
+        Fgen = results['F']
+
+        plt.figure()
+        n, b, p = plt.hist(Kgen, bins=20)
+        plt.xlabel('Kgen')
+        plt.ylabel('N')
+        plt.savefig('tritium_energies_histogram.png')
+
+        # re-configure poly_fitter
+        # config dict
+        config_dict = {
+            'order': 2,
+            'x_key': 'K',
+            'y_key': 'N',
+            'error_key': 'sqrt_N'
+            }
+        poly_fitter.Configure(config_dict)
+
+        bin_centers = b[0:-1]+0.5*(b[1]-b[0])
+        poly_fitter.data = {'K': bin_centers, 'N': n, 'sqrt_N': np.sqrt(n)}
+
+        # run and add fit result to previous plot
+        poly_fitter.Run()
+        tritium_fit_results = poly_fitter.results
+
+        plt.plot(tritium_fit_results['sorted_x'], tritium_fit_results['best_fit'], label='Fit result')
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig('tritium_polynomial_fit.png')
 
 
 
