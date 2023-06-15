abf8bf79a46decf22742129f6eec278a8a48c54e
cclaessens <claessens.christine@hotmail.de>
2023-06-15 09:36:51 -0700

    starting on exposure plot

--- tests/Sensitivity_test.py
+++ tests/Sensitivity_test.py
@@ -21,7 +21,7 @@ class SensitivityTest(unittest.TestCase):
             # required
             "config_file_path": "/termite/sensitivity_config_files/Config_PhaseIII_325MHz_Experiment.cfg",
             #"config_file_path": "/host_repos/sensitivity_branches/termite/sensitivity_config_files/Config_PhaseIII_325MHz_Experiment.cfg",
-            "plot_path": "./sensitivity_vs_efficiency_curve.pdf",
+            "plot_path": "./sensitivity_vs_exposure_curve.pdf",
             # optional
             "track_length_axis": False,
             "molecular_axis": False,
@@ -37,19 +37,19 @@ class SensitivityTest(unittest.TestCase):
             "comparison_curve_label": r"Atomic"+"\n"+r"10 $\times$ 3 years"+"\n"+r"$\sigma^\bar{B}_\mathrm{reco} = 0.07\,\mathrm{eV}$",
             "goals": {"Phase III (0.1 eV)": 0.1, "Phase IV (0.04 eV)": 0.04},
             "comparison_curve": True,
-            "comparison_config_file_path": "/termite/sensitivity_config_files/Config_atomic_325MHz_Experiment.cfg",
+            "comparison_config_file_path": ["/termite/sensitivity_config_files/Config_atomic_325MHz_Experiment.cfg"],
             #"config_file_path": "/host_repos/sensitivity_branches/termite/sensitivity_config_files/Config_atomic_325MHz_Experiment.cfg",
             #"B_inhomogeneity": np.linspace(0.1, 2.1, 10)*1e-6,
             #"B_inhom_uncertainty": 0.01,
-            "sigmaE_theta_r": np.linspace(0.07, 0.15, 10), #in eV, energy broadening from theta and r reconstruction
+            "sigmaE_theta_r": 0.159, #in eV, energy broadening from theta and r reconstruction
             "lower_label_y_position": 0.17,
             "upper_label_y_position": 0.7,
             "label_x_position": 0.015, #1.5e15, #0.02, #1e14,
             "goals_x_position": 0.0002 #2e12 #0.0002
             }
-        #sens_curve = CavitySensitivityCurveProcessor("sensitivity_curve_processor")
-        #sens_curve.Configure(sens_config_dict)
-        #sens_curve.Run()
+        sens_curve = CavitySensitivityCurveProcessor("sensitivity_curve_processor")
+        sens_curve.Configure(sens_config_dict)
+        sens_curve.Run()
         
         
         sens_config_dict = {
