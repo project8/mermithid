45105a71fe9c975e936e9422a0ae46c1ace0bff4
Talia Weiss <talia.weiss@yale.edu>
2023-06-22 14:57:26 -0400

    Performing tests

--- tests/Sensitivity_test.py
+++ tests/Sensitivity_test.py
@@ -58,9 +58,9 @@ class SensitivityTest(unittest.TestCase):
             "goals_x_position": 0.2e-10, #2e12 #0.0002
             "goals_y_rel_position": 0.4
             }
-        sens_curve = CavitySensitivityCurveProcessor("sensitivity_curve_processor")
-        sens_curve.Configure(sens_config_dict)
-        sens_curve.Run()
+        #sens_curve = CavitySensitivityCurveProcessor("sensitivity_curve_processor")
+        #sens_curve.Configure(sens_config_dict)
+        #sens_curve.Run()
         
         
         sens_config_dict = {
@@ -162,9 +162,9 @@ class SensitivityTest(unittest.TestCase):
             "goals_x_position": 1.2e12, 
             "plot_key_parameters": True
             }
-        #sens_curve = CavitySensitivityCurveProcessor("sensitivity_curve_processor")
-        #sens_curve.Configure(sens_config_dict)
-        #sens_curve.Run()
+        sens_curve = CavitySensitivityCurveProcessor("sensitivity_curve_processor")
+        sens_curve.Configure(sens_config_dict)
+        sens_curve.Run()
 
     def test_SensitivityProcessor(self):
         from mermithid.processors.Sensitivity import AnalyticSensitivityEstimation
