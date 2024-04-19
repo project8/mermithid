b5d7dc9970bbdba4086ed651ba0ac87ab31639fb
Talia Weiss <talia.weiss@yale.edu>
2024-04-19 09:32:48 -0400

    1 GHz and 500 MHz LFA comparison

--- test_analysis/LFA_Sensitivity.py
+++ test_analysis/LFA_Sensitivity.py
@@ -15,7 +15,7 @@ import numpy as np
 # Configuration for CCA Sensitivity vs. density plot
 sens_config_dict = {
     # required
-    "config_file_path": "/termite/sensitivity_config_files/Config_LFA_Experiment_1GHz.cfg", #"/termite/sensitivity_config_files/Config_LoverDof5_LFA_Experiment.cfg",
+    "config_file_path": "/termite/sensitivity_config_files/config_LFA_Experiment_500MHz.cfg", #Config_LFA_Experiment_1GHz.cfg", 
     #"config_file_path": "/host_repos/sensitivity_branches/termite/sensitivity_config_files/Config_PhaseIII_325MHz_Experiment.cfg",
     "plot_path": "./lfa_sensitivity_vs_density_curve.pdf",
     # optional
@@ -26,7 +26,7 @@ sens_config_dict = {
     "density_axis": True,
     "cavity": True,
     #"y_limits": [2e-2, 20],
-    "y_limits": [3e-1, 20],
+    "y_limits": [2e-1, 20],
     "density_range": [1e13,1e20],
     "efficiency_range": [0.0001, 1],
     #"density_range": [1e8, 1e12],
@@ -53,9 +53,9 @@ sens_config_dict = {
     "configure_sigma_theta_r": True,
     "sigmae_theta_r": np.linspace(0.1, 0.5, 10), #in eV, energy broadening from theta and r reconstruction
     }
-sens_curve = CavitySensitivityCurveProcessor("sensitivity_curve_processor")
-sens_curve.Configure(sens_config_dict)
-sens_curve.Run()
+#sens_curve = CavitySensitivityCurveProcessor("sensitivity_curve_processor")
+#sens_curve.Configure(sens_config_dict)
+#sens_curve.Run()
 
 
 sens_config_dict = {
@@ -63,42 +63,43 @@ sens_config_dict = {
     "config_file_path": "/termite/sensitivity_config_files/Config_LFA_Experiment_1GHz.cfg", #"/termite/sensitivity_config_files/Config_LFA_Experiment.cfg",
     "plot_path": "./lfa_sensitivity_vs_exposure_curve_1GHz.pdf",
     # optional
-    "figsize": (10,6),
+    "figsize": (8,6),
     "fontsize": 15,
-    "legend_location": "upper right",
+    "legend_location": "upper center",
     "track_length_axis": False,
     "molecular_axis": False,
     "atomic_axis": False,
     "density_axis": False,
     "cavity": True,
-    "add_PhaseII": False,
     "add_1year_1cav_point_to_last_ref": False,
-    "PhaseII_config_path": "/termite/sensitivity_config_files/Config_PhaseII_Experiment.cfg",
     "y_limits": [10e-3, 500],
     "density_range": [1e12,1e19],
     "exposure_range": [1e-11, 1e4],
-    "main_curve_upper_label": r"LFA",
+    "main_curve_upper_label": r"LFA (atomic T pilot) - 1 GHz",
     "main_curve_lower_label": r"$\sigma^\bar{B}_\mathrm{reco} = 0.16\,\mathrm{eV}$",
-    "goals": {"Phase III (0.2 eV)": (0.2**2/np.sqrt(1.64)), "Phase IV (0.04 eV)": (0.04**2/np.sqrt(1.64))},
+    "goals": {"Phase III (0.4 eV)": (0.4**2/np.sqrt(1.64)), "Phase IV (0.04 eV)": (0.04**2/np.sqrt(1.64))},
     "comparison_curve": True,
-    "comparison_config_file_path": ["/termite/sensitivity_config_files/Config_atomic_find_factor_22_Experiment_conservative.cfg"], 
+    "comparison_config_file_path": ["/termite/sensitivity_config_files/Config_LFA_Experiment_500MHz.cfg",
+                                    "/termite/sensitivity_config_files/Config_atomic_find_factor_22_Experiment_conservative.cfg"], 
     #"comparison_config_file_path": ["/termite/sensitivity_config_files/Config_PhaseIII_325MHz_Experiment_conservative.cfg", 
     #                                "/termite/sensitivity_config_files/Config_atomic_325MHz_Experiment_conservative.cfg", 
     #                                "/termite/sensitivity_config_files/Config_atomic_325MHz_Experiment.cfg"],
     #"comparison_curve_label": [r"Molecular, conservative", "Atomic, conservative", "Atomic, reaching PIV target"],
-    "comparison_curve_label": [r"Phase IV 150 MHz scenario"],
+    "comparison_curve_label": [r"LFA (atomic T pilot) - 500 MHz", r"Phase IV scenario - 150 MHz"],
     #"comparison_curve_colors": ["blue", "darkred", "red"],
-    "comparison_curve_colors": ["red"],
+    "comparison_curve_colors": ["darkred", "red"],
     "optimize_main_density": False,
     "lower_label_y_position": 0.17,
     "upper_label_y_position": 0.7,
     "label_x_position": 0.015, #1.5e15, #0.02, #1e14,
     "goals_x_position": 0.2e-10, #2e12 #0.0002
-    "goals_y_rel_position": 0.4
+    "goals_y_rel_position": 0.4,
+    "add_PhaseII": True,
+    "PhaseII_config_path": "/termite/sensitivity_config_files/Config_PhaseII_Experiment.cfg"
     }
-#sens_curve = CavitySensitivityCurveProcessor("sensitivity_curve_processor")
-#sens_curve.Configure(sens_config_dict)
-#sens_curve.Run()
+sens_curve = CavitySensitivityCurveProcessor("sensitivity_curve_processor")
+sens_curve.Configure(sens_config_dict)
+sens_curve.Run()
 
 
 
