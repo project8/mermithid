70dc13f26cacf977ec3a70197e150f39aac74c90
cclaessens <claessens.christine@hotmail.de>
2024-05-14 11:40:11 -0700

    added spinx_rtd_theme

--- documentation/conf.py
+++ documentation/conf.py
@@ -31,6 +31,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'spinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
