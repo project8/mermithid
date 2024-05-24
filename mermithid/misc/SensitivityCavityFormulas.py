Fixed miss-handling of units for outputting scan parameters

mermithid/misc/SensitivityCavityFormulas.py
@@ -460,7 +460,7 @@ class CavitySensitivity(object):
             pass
         print("Contribution to sigma_(m_beta^2)", " "*18, "%.2f"%(self.SystSens()/meV**2), "meV^2 ->", "%.2f"%(np.sqrt(self.SystSens())/meV), "meV")
         print("Systematic mass limit", " "*18, "%.2f"%(np.sqrt(1.28*self.SystSens())/meV), "meV")
-        return np.sqrt(1.28*self.SystSens())/meV, np.sqrt(np.sum(sigmas**2))
+        return np.sqrt(1.28*self.SystSens())/meV, np.sqrt(np.sum(sigmas**2))/meV
 
     def syst_doppler_broadening(self):
         # estimated standard deviation of Doppler broadening distribution from
@@ -557,7 +557,7 @@ class CavitySensitivity(object):
         logger.info("Received power: {}W".format(self.received_power/W))
         logger.info("Noise power in 1eV: {}W".format(self.noise_energy*eV_bandwidth/W))
         logger.info("Noise temperature: {}K".format(self.noise_temp/K))
-        logger.info("Opimtum energy window: {} eV".format(self.DeltaEWidth()/eV))
+        logger.info("Optimum energy window: {} eV".format(self.DeltaEWidth()/eV))
 
         return self.noise_temp/K, self.received_power/(self.noise_energy*eV_bandwidth), track_duration/ms
 

mermithid/processors/Sensitivity/SensitivityParameterScanProcessor.py
@@ -283,7 +283,7 @@ class SensitivityParameterScanProcessor(BaseProcessor):
         self.results = {"scan_parameter": self.scan_parameter_name, "scan parameter_unit": self.scan_parameter_unit_string,
                         "scan_parameter_values": self.scan_parameter_values, "optimum_limits_eV": np.array(self.optimum_limits)/eV,
                         "optimum_densities/m3": np.array(self.optimum_rhos)*(m**3),
-                        "Noise Temperatures/K": np.array(self.noise_temp)/K,
+                        "Noise Temperatures/K": np.array(self.noise_temp),
                         "SNRs 1eV from temperature": np.array(self.SNR), "track durations": np.array(self.track_duration)/ms,
                         "Systematic limits": np.array(self.sys_lim), "Total Sigmas": np.array(self.total_sigma)}
         