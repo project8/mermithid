fe1592d21fe61bba64c26942e2baa0bc943b4dcb
ManjinderOueslati <manjinderoueslati@gmail.com>
2026-01-30 19:57:24 -0500

    Fixed python environment for local repository and typo errors in Formulas

--- mermithid/sensitivity/SensitivityCavityFormulas.py
+++ mermithid/sensitivity/SensitivityCavityFormulas.py
@@ -257,9 +257,9 @@ class CavitySensitivity(Sensitivity):
 
         #Ioffe bite used in radial efficiency and effective volume calculation
         self.unusable_dist_from_wall = self.Efficiency.unusable_dist_from_wall
-        if self.Effficiency.calculate_ioffe_bite and all(hasattr(self.MagneticField, attr) for attr in ("nominal_field", "magnetic_inhomogenity", "ioffe_field", "ioffe_multipolarity")):
+        if self.Efficiency.calculate_ioffe_bite and all(hasattr(self.MagneticField, attr) for attr in ("nominal_field", "magnetic_inhomogenity", "ioffe_field", "ioffe_multipolarity")):
             self.unusable_dist_from_wall = ioffe_bite(self.MagneticField.nominal_field, self.MagneticField.magnetic_inhomogenity, self.MagneticField.ioffe_field, self.MagneticField.ioffe_multipolarity, self.cavity_radius)
-        elif calculate_ioffe_bite:
+        elif self.Efficiency.calculate_ioffe_bite:
             logger.info("Error: Did not specify all attributes for Ioffe bite calculation in config file")
 
         #Assigning the background constant if it's not in the config file
